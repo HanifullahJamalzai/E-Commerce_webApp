10309338faf10039d212802572fbce30d630a960
Hanifullah Jamalzai <hanifullahjamalzai2014@gmail.com>
2022-02-09 10:28:32 +0430

    ProductList Api Done

---
+++ admin/app/Http/Controllers/Admin/ProductListController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\ProductList;
+
+class ProductListController extends Controller
+{
+    public function ProductListByRemark(Request $request){
+        $remark = $request->remark;
+        $productList = ProductList::where('remark', $remark)->get();
+        return $productList;
+    } //End Method
+    
+    public function ProductListByCategory(Request $request){
+        $category = $request->category;
+        $productList = ProductList::where('category', $category)->get();
+        return $productList;
+    } //End Method
+
+    public function ProductListBySubCategory(Request $request){
+        $category = $request->category;
+        $subcategory = $request->subcategory;
+        $productList = ProductList::where('category', $category)->where('subcategory', $subcategory)->get();
+        return $productList;
+    } //End Method
+}
--- admin/routes/api.php
+++ admin/routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Admin\VisitorController;
 use App\Http\Controllers\Admin\ContactController;
 use App\Http\Controllers\Admin\SiteInfoController;
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\ProductListController;
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
@@ -19,3 +20,7 @@ Route::post('/postcontact', [ContactController::class, 'PostContactDetails']);
 Route::get('/allsiteinfo', [SiteInfoController::class, 'AllSiteInfo']);
 // All Category Route
 Route::get('/allcategory', [CategoryController::class, 'AllCategory']);
+// ProductList Route
+Route::get('/productlistbyremark/{remark}', [ProductListController::class, 'ProductListByRemark']);
+Route::get('/productlistbycategory/{category}', [ProductListController::class, 'ProductListByCategory']);
+Route::get('/productlistbysubcategory/{category}/{subcategory}', [ProductListController::class, 'ProductListBySubCategory']);
